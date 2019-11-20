d95d1fce87af24dcc98791f2c8ac6783aadd3373
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2019-11-20 14:19:58 +0100

    -init repo-

---
+++ src/main/java/pl/put/poznan/buildinginfo/app/BuildinginfoApplication.java
@@ -0,0 +1,13 @@
+package pl.put.poznan.buildinginfo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildinginfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildinginfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/put/poznan/buildinginfo/BuildinginfoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.put.poznan.buildinginfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildinginfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
