79b83bf2e429f6303756d1abec546e5b21ab84f0
Błażej <baryla181@gmail.com>
2019-11-24 21:39:52 +0100

    Dodanie Loggera SLF4J

--- src/main/java/pl/put/poznan/buildinginfo/app/BuildinginfoApplication.java
+++ src/main/java/pl/put/poznan/buildinginfo/app/BuildinginfoApplication.java
@@ -1,13 +1,17 @@
 package pl.put.poznan.buildinginfo.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BuildinginfoApplication {
-
+	private static Logger log = LoggerFactory.getLogger(BuildinginfoApplication.class);
 	public static void main(String[] args) {
+		log.info("Creating Spring Project");
 		SpringApplication.run(BuildinginfoApplication.class, args);
+
 	}
 
 }
