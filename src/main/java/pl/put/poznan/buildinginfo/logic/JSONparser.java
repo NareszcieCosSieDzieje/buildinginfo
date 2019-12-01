3253d1f50c4329a5db3d3538dd9be13917de8e83
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2019-12-01 20:39:24 +0100

    -datagenerator upgrade-

--- src/main/java/pl/put/poznan/buildinginfo/logic/JSONparser.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/JSONparser.java
@@ -3,6 +3,7 @@ package pl.put.poznan.buildinginfo.logic;
 import com.google.gson.Gson;
 
 public class JSONparser {
+
     public static Building getObject(String buildingData)
     {
         Gson gson = new Gson();
