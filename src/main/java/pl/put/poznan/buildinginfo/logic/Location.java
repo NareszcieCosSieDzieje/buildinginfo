29c4ab7cf749963074cb43e6adaf9cfc5f2be16e
Kamil Kowalczyk <kamkow10@wp.pl>
2019-12-03 15:51:34 +0100

    Added NameGenerator for Location class

--- src/main/java/pl/put/poznan/buildinginfo/logic/Location.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Location.java
@@ -1,19 +1,22 @@
 package pl.put.poznan.buildinginfo.logic;
 
+import java.util.Random;
 import java.util.UUID;
 
 public abstract class Location {
     private String id;
     private String name;
 
-    public Location(){
-        this("RANDOMLY_GENERATED");
-    }
-
-    public Location(String name){
+    public Location(String name) {
+        if (name == null) {
+            name = this.generateRandomName();
+        }
         this.name = name;
         this.id = UUID.randomUUID().toString();
-        //Metoda generowania/sprawdzania nazw>
+    }
+
+    public Location() {
+        this(null);
     }
 
     public void setId(String id) { this.id = id; }
@@ -25,6 +28,23 @@ public abstract class Location {
     }
 
     public String getName() {
+        return name;
+    }
+
+    private String generateRandomName() {
+        Random generator = new Random();
+        String name = "";
+        boolean upper;
+        char letter;
+        for(int i = 0; i < 10; i++) {
+            upper = generator.nextBoolean();
+            if (upper) {
+                letter = (char)(generator.nextInt(26) + 65);
+            } else {
+                letter = (char)(generator.nextInt(26) + 97);
+            }
+            name += letter;
+        }
         return name;
     }
 }
